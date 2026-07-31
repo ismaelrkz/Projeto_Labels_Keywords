ad25aecbf9250f3231bc3486d6a600e791df83c7
Ismael Reckziegel <ismaelreckziegel@Ismaels-MacBook-Air.local>
2026-07-31 10:30:21 -0300

    feat: adiciona projeto Spring Boot com Maven e configuracoes de ambiente

---
+++ Labels_Keywords/src/main/java/br/ismaelreckziegel/labels_keywords/LabelsKeywordsApplication.java
@@ -0,0 +1,13 @@
+package br.ismaelreckziegel.labels_keywords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelsKeywordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabelsKeywordsApplication.class, args);
+    }
+
+}
---
+++ Labels_Keywords/src/test/java/br/ismaelreckziegel/labels_keywords/LabelsKeywordsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ismaelreckziegel.labels_keywords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelsKeywordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
